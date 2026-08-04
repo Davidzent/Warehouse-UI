d07d8de7aaaa12433fa4518f921a858a17386194
davidzent <davidgin641@gmail.com>
2026-08-04 11:40:27 -0700

    Added warehouse to the deploy
    Signed-off-by: davidzent <davidgin641@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/warehouse/',
+
+  server: {
+    port: Number(process.env.PORT) || 5176,
+  },
+
+  build: {
+    outDir: '../../dist/warehouse',
+    emptyOutDir: false,
+  },
 })
